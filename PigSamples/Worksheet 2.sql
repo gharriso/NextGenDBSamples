62b3a8e8c51b6ef65f4a946d9a69b9c4709ab26b
gharriso <guy.a.harrison@gmail.com>
2015-10-14 08:46:05 +1100

    Pig Samples

---
+++ PigSamples/Worksheet 2.sql
@@ -0,0 +1,8 @@
+  SELECT country_name , COUNT (cust_id)
+    FROM countries JOIN customers USING (country_id)
+   WHERE region = 'Asia'
+GROUP BY country_name 
+  HAVING COUNT (cust_id) > 500
+  ORDER BY COUNT(cust_id) DESC 
+
+
