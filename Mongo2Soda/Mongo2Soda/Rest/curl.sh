9e6c44bac77d057096dde70809cd71e53dd1da8f
gharriso <guy.a.harrison@gmail.com>
2015-10-31 20:34:46 -0700

    Mongo Soda

---
+++ Mongo2Soda/Mongo2Soda/Rest/curl.sh
@@ -0,0 +1,21 @@
+set -vx
+#Retrieve document by ID 
+curl http://oraclehost:8080/ords/opsg/soda/latest/ofilms/3
+
+
+#Post a query 
+cat>query1.json  <<EOF
+{ "Title" : "BREAKING HOME" }
+
+EOF
+
+
+curl -X POST --data-binary query1.json -H "Content-Type: application/json" http://oraclehost:8080/ords/opsg/soda/latest/ofilms?action=query
+
+#Post a more complex query 
+cat>query2.json <<EOF
+{ $and : [ { Length" : { $gte : 120}},{"Rating" : "PG-13"} ] }
+
+EOF
+
+curl -X POST --data-binary query2.json -H "Content-Type: application/json" http://oraclehost:8080/ords/opsg/soda/latest/ofilms?action=query
